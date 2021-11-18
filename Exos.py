0c859489f61105e0ba4c72393c0b17d48ff40529
l1t-w1n <lmv06013@gmail.com>
2021-11-18 11:16:57 +0100

    ex 10

--- Exos.py
+++ Exos.py
@@ -1,6 +1,7 @@
 import itertools as it
 from pylab import *
 from random import *
+from collections import deque
 c=0
 def naif2(t):
 	max=t[0]
@@ -70,4 +71,92 @@ def bancEssai():
 		data.append(c/2**i)
 	afficherCourbe(data)
 
-bancEssai()
+def pileVide():
+	return []
+def estVide(P):
+	return not P
+def empiler(P,n):
+	return P.append(n)
+def depiler(p):
+	return p.pop()
+def sommet(p):
+	return p[-1]
+def depiler2(p):
+	q=deque(p)
+	return q.popleft()
+
+def inverse(p):
+	res=pileVide()
+	save=pileVide()
+	if estVide(p):
+		return res
+	else:
+		while not estVide(p):
+			e=depiler(p)
+			empiler(res,e)
+			empiler(save,e)
+		while not estVide(save):
+			empiler(p,depiler(save))
+		return res
+
+def copie(p):
+	res=pileVide()
+	save=pileVide()
+	save2=pileVide()
+	if estVide(p):
+		return p
+	else:
+		while not estVide(p):
+			e=depiler(p)
+			empiler(save,e)
+			empiler(save2,e)
+		inverse(save)
+		while not estVide(save):
+			e=depiler(save)
+			empiler(res,e)
+		while not estVide(save2):
+			e=depiler(save2)
+			empiler(res,e)
+			empiler(p,e)	
+		return res
+
+def copieElem(p):
+	res=pileVide()
+	save=pileVide()
+	save2=pileVide()
+	save3=pileVide()
+	if estVide(p):
+		return p
+	else:
+		while not estVide(p):
+			e=depiler(p)
+			empiler(save,e)
+			empiler(save2,e)
+			empiler(save3,e)
+		inverse(save)
+		inverse(save2)
+		while not estVide(save3):
+			empiler(res,depiler(save))
+			empiler(res,depiler(save2))
+			empiler(p,depiler(save3))
+		return res
+		
+def double(p):
+	res=pileVide()
+	save=pileVide()
+	save2=pileVide()
+	if estVide(p):
+		return p
+	else:
+		while not estVide(p):
+			e=depiler(p)
+			empiler(save,e)
+			empiler(save2,e)
+		inverse(save)
+		while not estVide(save):
+			e=depiler(save)
+			empiler(res,e*2)
+			empiler(p,e)	
+		return res
+k=[1,2,3]
+print(double(k))
